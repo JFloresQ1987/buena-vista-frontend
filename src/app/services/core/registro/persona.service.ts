6a20009bca2a3534294ac147c657ef8a65bce910
andy <andy.luya.27@unsch.edu.pe>
2020-08-30 11:26:28 -0500

    Actualizar socio

--- src/app/services/core/registro/persona.service.ts
+++ src/app/services/core/registro/persona.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
+
+import { map } from 'rxjs/operators';
+
 import { Persona } from '../../../interfaces/core/registro/persona.interface';
 
 const base_url = environment.base_url;
@@ -12,11 +15,26 @@ export class PersonaService {
 
   constructor(private http: HttpClient) {
     console.log('entro para buscando persona')
-   }
-
-  crear(objeto: Persona){
-
-    const url = `${ base_url }/personas`;
-    return this.http.post(url, objeto);
   }
+
+   
+  crear(objeto: Persona){
+    const url = `${ base_url }/personas`;
+    return this.http.post(url, objeto)
+  }
+
+  obtenerPersona(id: String){
+    const url = `${ base_url }/personas/${id}`;
+    return this.http.get(url)
+                .pipe(
+                  map((resp: {ok: boolean, persona: Persona}) => resp.persona)
+                )
+  }
+    
+  actualizar(objeto: Persona){
+    const url = `${ base_url }/personas/${objeto.id}`;
+    return this.http.put(url, objeto);
+  }  
+  
+
 }
